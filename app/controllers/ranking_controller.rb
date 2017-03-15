cc85cbcd89c452d370681dd5a9d671b79497f1f9
takashi matsui <takabamboo18@gmail.com>
2017-03-15 10:36:31 +0000

    変更2

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,15 +1,14 @@
 class RankingController < ApplicationController
   def have
     @title = "Have ランキング"
-    counts = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
-    @counts = counts.values
-    ids = counts.keys
+    @counts = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    ids = @counts.keys
     @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
     render 'ranking'
   end
 
   def want
-    @title = "Wantランキング"
+    @title = "Want ランキング"
     @counts = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
     ids = @counts.keys
     @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
